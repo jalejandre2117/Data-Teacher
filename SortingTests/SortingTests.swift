d9efae660ec7c58b5cdfb606c4a39b215a308a19
Jorge Alejandre <jorge.alejandre2117@gmail.com>
2019-01-31 15:11:58 -0500

    Initial Commit

---
+++ SortingTests/SortingTests.swift
@@ -0,0 +1,34 @@
+//
+//  SortingTests.swift
+//  SortingTests
+//
+//  Created by Jorge Alejandre on 1/31/19.
+//  Copyright © 2019 Jorge Alejandre. All rights reserved.
+//
+
+import XCTest
+@testable import Sorting
+
+class SortingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
